007d976f109cdd90b983ce3c62e7d07aedbc1689
Sean Trinh <sean.a.trinh@gmail.com>
2022-06-28 14:03:24 -0700

    jump game ii

---
+++ jumpGame2.js
@@ -0,0 +1,23 @@
+function jumpGame2(array) {
+  let jumps = 0;
+  let steps = array[0];
+  let maxSteps = array[0];
+
+  for (let i = 1; i < array.length - 1; i++) {
+    steps--;
+    maxSteps = Math.max(maxSteps, i + array[i]);
+
+    if (steps === 0) {
+      steps = maxSteps - i;
+      jumps++;
+    }
+  }
+
+
+  return jumps + 1;
+}
+
+// tests
+
+const test1 = [2, 3, 1, 1, 4];
+console.log(jumpGame2(test1)); // 2

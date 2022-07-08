b077cca37b6c705875875a1547b70bf329f53d18
Sean Trinh <sean.a.trinh@gmail.com>
2022-07-07 22:02:54 -0700

    remove kth node from end of linked list

---
+++ algos/removeKthNodeFromEnd.js
@@ -0,0 +1,30 @@
+function removeKthNodeFromEnd(head, k) {
+  let first = head;
+  let second = head;
+  let secondMoves = 0;
+  let nodeBeforeRemoval;
+
+  while (secondMoves < k) {
+    second = second.next;
+    secondMoves++;
+  }
+
+  if (second === null) {
+    head.value = head.next.value;
+    head.next = head.next.next;
+    return first;
+  }
+
+  while (second) {
+    if (second.next === null) {
+      nodeBeforeRemoval = first;
+    }
+    first = first.next;
+    second = second.next;
+  }
+
+  nodeBeforeRemoval.next = first.next;
+  first.next = null;
+
+  return head;
+}

c386894ee40b03baa5b0abf7f0bf629b522f552d
Sean Trinh <sean.a.trinh@gmail.com>
2022-07-11 11:34:38 -0700

    merge two sorted lists

---
+++ algos/mergeSortedLists.js
@@ -0,0 +1,25 @@
+var mergeTwoLists = function(list1, list2) {
+  let head = new ListNode();
+  let main = head;
+
+  let current1 = list1;
+  let current2 = list2;
+
+  while (current1 && current2) {
+      if (current1.val <= current2.val) {
+          main.next = current1;
+          current1 = current1.next;
+      } else {
+          main.next = current2;
+          current2 = current2.next;
+      }
+      main = main.next;
+  }
+  if (current1) {
+      main.next = current1;
+  } else if (current2) {
+      main.next = current2;
+  }
+
+  return head.next;
+};

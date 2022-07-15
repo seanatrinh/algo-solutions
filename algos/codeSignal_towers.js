1adf2859265160d096480076d636186ee4ab9c99
Sean Trinh <sean.a.trinh@gmail.com>
2022-07-15 12:40:46 -0700

    codesignal towers question

---
+++ algos/codeSignal_towers.js
@@ -0,0 +1,12 @@
+/*
+given an array of integers, towers, calcualte how many increases/decrease you need to make per tower to make the towers array
+consecutively increasing or consecutively decreasing
+
+[1, 4, 3, 2] // return 4, since you need to add 4 to index 0 to make the array [5, 4, 3, 2]
+
+[5, 7, 9, 10, 11] // return 3, since you need to add 2 to index 0 and 1 to index 1 to make the array [7, 8, 9, 10, 11]
+*/
+
+function makeConsecutive(towers) {
+
+}
